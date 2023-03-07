5517cbb972549a773afee3a355924e8a08291813
María Rosa Sánchez <maria.z.sanchez.-nd@disney.com>
2023-03-06 22:44:21 -0600

    Add Breakpoints to progress bar

--- src/progress_bar_exercise/components/Solution/Solution.js
+++ src/progress_bar_exercise/components/Solution/Solution.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Button from "../../../components/Button";
 import ProgressBar from "../../../components/ProgressBar";
+import Breakpoints from "../Breakpoints/Breakpoints";
 import "./Solution.scss";
 
 const HANGING_BREAKPOINT = 90;
@@ -10,18 +11,37 @@ const Solution = () => {
   const [isRequestActive, setIsRequestActive] = useState(false);
   const [, setIsRequestCompleted] = useState(false);
   const [currentProgress, setCurrentProgress] = useState(0);
+  const [useBreakpoints, setUseBreakpoints] = useState(false);
+  const [breakpoints, setBreakpoints] = useState([]);
 
   useEffect(() => {
     if (isRequestActive && currentProgress < HANGING_BREAKPOINT) {
       const timer = setInterval(() => {
-        setCurrentProgress(
-          (prevCurrentProgress) => prevCurrentProgress + PROGRESS_PER_SECOND
-        );
+        setCurrentProgress((prevCurrentProgress) => {
+          const newCurrentProgress = prevCurrentProgress + PROGRESS_PER_SECOND;
+          const newBreakpointCurrentProgress = prevCurrentProgress + PROGRESS_PER_SECOND / 2;
+          let isBreakpoint = false;
+
+          if (useBreakpoints) {
+            isBreakpoint = breakpoints.some(
+              (breakpoint) =>
+                breakpoint >= prevCurrentProgress &&
+                breakpoint <= newCurrentProgress
+            );
+          }
+
+          if (isBreakpoint && newBreakpointCurrentProgress < HANGING_BREAKPOINT) {
+            return newBreakpointCurrentProgress
+          } else if (!isBreakpoint && newCurrentProgress < HANGING_BREAKPOINT) {
+            return newCurrentProgress;
+          }
+          return HANGING_BREAKPOINT;
+        });
       }, 1000);
 
       return () => clearTimeout(timer);
     }
-  }, [isRequestActive, currentProgress]);
+  }, [isRequestActive, currentProgress, useBreakpoints, breakpoints]);
 
   const handleStartRequest = () => {
     if (!isRequestActive) {
@@ -37,9 +57,30 @@ const Solution = () => {
     setCurrentProgress(100);
   };
 
+  const toggleBreakpoints = () => {
+    setUseBreakpoints((prevUseBreakpoints) => !prevUseBreakpoints);
+  };
+
+  const updateBreakpoints = (values) => {
+    const newBreakpoints = values
+      .replace(" ", "")
+      .split(",")
+      .map((value) => parseInt(value, 10))
+      .filter((value) => value > 0 && value < HANGING_BREAKPOINT)
+      .sort((a, b) => a - b);
+    setBreakpoints(newBreakpoints);
+  };
+
   return (
     <div className="solution">
       <ProgressBar progress={currentProgress} />
+      <Breakpoints
+        breakpoints={breakpoints}
+        useBreakpoints={useBreakpoints}
+        onToggleBreakpoints={toggleBreakpoints}
+        onUpdateBreakpoints={updateBreakpoints}
+        disabled={isRequestActive}
+      />
       <div className="solution__actions">
         <Button
           color="success"
